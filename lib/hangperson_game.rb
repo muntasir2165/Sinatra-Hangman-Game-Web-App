d8b4af31bc92be58615f2e304ca590541982978c
muntasir2165 <muntasir2165@hotmail.com>
2017-06-26 21:04:41 +0000

    Finished implementing the hangperson_game.rb class and the associated logic

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,12 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -18,5 +21,49 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+  
+  def guess(char)
+    #check if char is nil, empty or a non-alphabetic character
+    if (char.nil? or char.empty? or (char =~ /[^a-zA-Z]/) != nil)
+      raise ArgumentError.new("Invalid argument of type nil, empty string or non-alphabetic character")
+    end
+    
+    #check if char has already been guesses
+    if ((@guesses.include?(char.downcase)) or (@wrong_guesses.include?(char.downcase)))
+      return false
+    end
+    
+    if @word.downcase.include?(char.downcase)
+      @guesses += char.downcase
+    else
+      @wrong_guesses += char.downcase
+    end
+    
+    return true
+  end
 
+  def word_with_guesses
+    display = ''
+    word.downcase.each_char do |char|
+      if @guesses.downcase.include?(char)
+        display += char
+      else
+        display += '-'
+      end
+    end
+    return display
+  end
+  
+  def check_win_or_lose
+    display = word_with_guesses
+    if @wrong_guesses.length >= 7
+      return :lose
+    end
+    #else
+    if display.include?('-')
+      return :play
+    else
+      return :win
+    end
+  end
 end
