1d79ddcc110463d92aeb21751c1bba7c34e66899
muntasir2165 <muntasir2165@hotmail.com>
2017-06-28 15:37:24 +0000

    implemented the /win and /lose routes and the associated logic

--- app.rb
+++ app.rb
@@ -59,17 +59,35 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      session[:state] = :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+    session[:state] = :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if session[:state].nil? #stop the user from going to the /win page directly without playing the game
+      redirect '/new'
+    else
+      session.delete(:state)
+      erb :win # You may change/remove this line
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if session[:state].nil? #stop the user from going to the /lose page directly without playing the game
+      redirect '/new'
+    else
+      session.delete(:state)
+      erb :lose # You may change/remove this line
+    end
   end
   
 end
